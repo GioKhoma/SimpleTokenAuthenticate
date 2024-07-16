731f72389a0ed0e0abd49e395ea116c3c9ad4cb1
giokhomaa <giokhomaa@gmail.com>
2024-07-17 01:10:15 +0400

    Add auth and register

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import UserAuth
+from .views import UserAuth, RegisterView, LoginView, ProfileView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', UserAuth.as_view(), name='user-auth'),
+    path('register/', RegisterView.as_view(), name='user-register'),
+    path('login/', LoginView.as_view(), name='user-login'),
+    path('profile/', ProfileView.as_view(), name='user-profile'),
     path('api-token-auth/', obtain_auth_token),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ProfileView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({'username': user.username, 'email': user.email})
+
 
 class UserAuth(APIView):
     pass
